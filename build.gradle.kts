98460ed0978399f30919f1f5f160da073bc45fb9
floren <florendg@gmail.com>
2020-12-09 07:36:38 +0100

    Fix deprication in build

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("io.grpc:grpc-stub:1.34.0")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
 
-    runtime("io.grpc:grpc-netty-shaded:1.34.0")
+    runtimeOnly("io.grpc:grpc-netty-shaded:1.34.0")
 
     testImplementation("junit:junit:4.13")
     testImplementation("io.grpc:grpc-testing:1.34.0")
